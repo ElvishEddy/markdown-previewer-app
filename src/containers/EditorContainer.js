743358bfddde4bcfb5bb70d6d5109b2801cd4869
unknown <anhlct@vmodev.com>
2022-02-15 14:51:38 +0700

    add new file

---
+++ src/containers/EditorContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Editor from "../components/Editor";
+
+function EditorContainer() {
+  return (
+    <>
+      <Editor />
+    </>
+  );
+}
+
+export default EditorContainer;
---
+++ src/containers/PreviewerContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Previewer from "../components/Previewer";
+
+function PreviewerContainer() {
+  return (
+    <>
+      <Previewer />
+    </>
+  );
+}
+
+export default PreviewerContainer;
